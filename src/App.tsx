2d117b44b0efc2b6b0616c1c8d5d11bb1c04dc26
hplaptop <hplaptop@gmail.com>
2024-11-13 12:09:41 +0530

    Navbar and Footer added for HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
+import Navbar from "./components/Navbar/Navbar"
+import Footer from "./components/Navbar/Footer/Footer"
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="">
+        <Navbar />
+      </div>
+      <div>
+        <Footer />
+      </div>
     </>
   )
 }
---
+++ src/components/Navbar/Footer/Footer.tsx
@@ -0,0 +1,305 @@
+const Footer = () => {
+    return (
+        <>
+            <div
+                id="w-node-_8e913dbd-084f-928f-f996-e34233002883-33002883"
+                className="w-layout-layout quick-stack-2 wf-layout-layout"
+                style={{
+                    opacity: 1,
+                    transform:
+                        "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+                    transformStyle: "preserve-3d"
+                }}
+            >
+                <div
+                    id="w-node-_8e913dbd-084f-928f-f996-e34233002884-33002883"
+                    className="w-layout-cell cell-15"
+                >
+                    <img
+                        // src="https://cdn.prod.website-files.com/65fc1876777e6eaf167fad43/660c16e63dc8e9d9633cd098_logo-horizontal.svg"
+                        src="https://cmogujarat.gov.in/sites/default/files/ft_logo.png"
+                        loading="lazy"
+                        alt=""
+                        className="w-[100px] h-[100px]"
+                    />
+                </div>
+                <div
+                    id="w-node-_8e913dbd-084f-928f-f996-e34233002886-33002883"
+                    className="w-layout-cell cell-10"
+                >
+                    <div className="block-links-1">
+                        <div className="footer-descriptor">SITEMAP</div>
+                        <a href="/" aria-current="page" className="footer-link w--current">
+                            Home
+                        </a>
+                        <a href="/technology" className="footer-link">
+                            Technology
+                        </a>
+                        <a href="/offering" className="footer-link">
+                            Offering
+                        </a>
+                        <a href="/careers" className="footer-link">
+                            Careers
+                        </a>
+                        <a href="/about" className="footer-link">
+                            About Us
+                        </a>
+                        <a href="/news" className="footer-link">
+                            News
+                        </a>
+                        <a href="/contact" className="footer-link">
+                            Contact
+                        </a>
+                    </div>
+                    <div className="block-links-2">
+                        <div className="links21">
+                            <div className="footer-descriptor">SOCIALS</div>
+                            <a href="https://twitter.com/DePolySA" className="footer-link">
+                                x / twitter
+                            </a>
+                            <a href="https://www.facebook.com/depoly.sa" className="footer-link">
+                                facebook
+                            </a>
+                            <a href="https://www.instagram.com/depoly.sa" className="footer-link">
+                                Instagram
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/company/depoly/"
+                                className="footer-link"
+                            >
+                                Linkedin
+                            </a>
+                        </div>
+                        <div className="links22">
+                            <div className="footer-descriptor">Media</div>
+                            <a href="#" className="footer-link values">
+                                Our Values []
+                            </a>
+                            <a href="/press" className="footer-link">
+                                Press
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    id="w-node-_8e913dbd-084f-928f-f996-e342330028ab-33002883"
+                    className="w-layout-cell cell-13"
+                >
+                    <div className="footer-descriptor">Locations</div>
+                    <div
+                        id="w-node-_8e913dbd-084f-928f-f996-e342330028ae-33002883"
+                        className="locations-holder"
+                    >
+                        <p className="paragraph-5">
+                            <strong>
+                                DePoly SA –HQ
+                                <br />
+                            </strong>
+                            Rue de l'Industrie 23, 1950 Sion, Valais, Switzerland
+                        </p>
+                        <p className="paragraph-5">
+                            <strong>
+                                DePoly SA –Showcase Plant
+                                <br />
+                            </strong>
+                            Route de l'Ile au Bois, 1870 Monthey, Valais, Switzerland
+                        </p>
+                    </div>
+                </div>
+                <div
+                    id="w-node-_8e913dbd-084f-928f-f996-e342330028be-33002883"
+                    className="w-layout-cell cell-11"
+                >
+                    <p className="paragraph-3">
+                        DePoly’s technology recycles unsorted, dirty PET waste into virgin-grade
+                        raw materials. Those PET precursors, PTA and MEG, are used by the industry
+                        to produce new plastics without the need for fossil fuels, while diverting
+                        valuable materials from landfills and incineration.
+                    </p>
+                </div>
+                <div
+                    id="w-node-aabfd10c-dee2-dae9-c67b-cb8591bc2ebc-33002883"
+                    className="w-layout-cell cell-17"
+                >
+                    <div className="project-footer">
+                        <div className="footer-descriptor last-row">INTERESTED?</div>
+                        <a href="/contact" className="cta-button footer-button w-button">
+                            Contact us
+                        </a>
+                    </div>
+                </div>
+                <div
+                    id="w-node-abb77c70-cbb5-9d83-2a79-37136a469567-33002883"
+                    className="w-layout-cell cell-18"
+                >
+                    <div
+                        id="w-node-a4086dc6-0997-2a34-c990-0bc3bf4ab120-33002883"
+                        className="careers-title align footer-news"
+                        style={{
+                            opacity: 1,
+                            transform:
+                                "translate3d(0px, 0px, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+                            transformStyle: "preserve-3d"
+                        }}
+                    >
+                        <div className="dot-block empty no-padding no-margin footer-dot" />
+                        <div className="decsriptor no-margin">Subscribe</div>
+                    </div>
+                </div>
+                <div
+                    id="w-node-a5e6cda9-69be-84af-4e46-e6cef37d05c4-33002883"
+                    className="w-layout-cell cell-19"
+                >
+                    <div
+                        id="w-node-_56d99e0b-703c-f007-ca77-cd6afa5f975f-33002883"
+                        className="newsletter-holder"
+                    >
+                        <h3 className="heading-53 footer-news-font">
+                            Our newsletters are a concentrated blast of all the latest insights from
+                            DePoly.
+                        </h3>
+                        <p className="paragraph-26">No spam – Unsubscribe anytime.</p>
+                        <div
+                            id="w-node-_56d99e0b-703c-f007-ca77-cd6afa5f9764-33002883"
+                            className="w-form"
+                        >
+                            <form
+                                id="wf-form-Newsletter"
+                                name="wf-form-Newsletter"
+                                data-name="Newsletter"
+                                method="get"
+                                className="newsletter-form"
+                                data-wf-page-id="65fc1876777e6eaf167fad4e"
+                                data-wf-element-id="56d99e0b-703c-f007-ca77-cd6afa5f9765"
+                                aria-label="Newsletter"
+                            >
+                                <div className="newsletter-fields">
+                                    <div className="input-fields">
+                                        <input
+                                            className="text-field first-name w-input"
+                                            maxLength={256}
+                                            name="name-2"
+                                            data-name="Name 2"
+                                            placeholder="First Name"
+                                            type="text"
+                                            id="name-2"
+                                            required={true}
+                                        />
+                                        <input
+                                            className="text-field email-address footer-email-field w-input"
+                                            maxLength={256}
+                                            name="email-2"
+                                            data-name="Email 2"
+                                            placeholder="Email Address"
+                                            type="email"
+                                            id="email-2"
+                                            required={true}
+                                        />
+                                    </div>
+                                    <input
+                                        type="submit"
+                                        data-wait="Please wait..."
+                                        className="cta-button footer-button w-button"
+                                        defaultValue="Subscribe"
+                                    />
+                                </div>
+                                <label className="w-checkbox checkbox-field">
+                                    <input
+                                        type="checkbox"
+                                        name="checkbox-2"
+                                        id="checkbox-2"
+                                        data-name="Checkbox 2"
+                                        required={true}
+                                        className="w-checkbox-input checkbox"
+                                    />
+                                    <span className="checkbox-label w-form-label" htmlFor="checkbox-2">
+                                        By subscribing you accept our{" "}
+                                        <a
+                                            href="http://depoly.cc/legal/privacy-policy"
+                                            className="link-8"
+                                        >
+                                            Privacy Policy
+                                        </a>
+                                    </span>
+                                </label>
+                            </form>
+                            <div
+                                className="success-message-2 w-form-done"
+                                tabIndex={-1}
+                                role="region"
+                                aria-label="Newsletter success"
+                            >
+                                <div className="text-block-5">
+                                    Thank you! You have been subscirbed to our newsletter - don't forget
+                                    to confirm it!
+                                </div>
+                            </div>
+                            <div
+                                className="error-message-2 w-form-fail"
+                                tabIndex={-1}
+                                role="region"
+                                aria-label="Newsletter failure"
+                            >
+                                <div className="text-block-6">
+                                    Oops! Something went wrong while submitting the form. Please try
+                                    again.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    id="w-node-_8e913dbd-084f-928f-f996-e342330028ce-33002883"
+                    className="w-layout-cell cell-16"
+                >
+                    <p className="small year">DePoly © 2024</p>
+                    <div className="legal-holder">
+                        <a
+                            href="http://www.depoly.co/legal/cookies-policy"
+                            className="footer-link small first-terms"
+                        >
+                            Cookies Policy
+                        </a>
+                        <a
+                            href="http://www.depoly.co/legal/privacy"
+                            className="footer-link small"
+                        >
+                            Privacy Policy
+                        </a>
+                        <a href="#" className="footer-link small hide-one">
+                            Privacy Policy
+                        </a>
+                    </div>
+                    <div className="language-bloxk">
+                        <div className="locales-wrapper w-locales-list">
+                            <div role="list" className="locales-list w-locales-items">
+                                <div role="listitem" className="w-locales-item">
+                                    <a
+                                        hrefLang="en"
+                                        href="/"
+                                        aria-current="page"
+                                        className="footer-link small language fr w--current"
+                                    >
+                                        EN
+                                    </a>
+                                </div>
+                                <div role="listitem" className="w-locales-item">
+                                    <a
+                                        hrefLang="fr-CH"
+                                        href="/fr"
+                                        className="footer-link small language fr"
+                                    >
+                                        FR
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+
+export default Footer
